f913f04588ff095731c24acab1af2b100119706b
5tr0bl <m.strobl@campus.tu-berlin.de>
2021-05-20 20:49:22 +0200

    Main file prototype

---
+++ granular_synth.c
@@ -0,0 +1,10 @@
+/**
+ * @file granular_synth.c
+ * @author Nikita Kretschmar, Adrian Philipp, Micha Strobl, Tim Wennemann <br>
+ * Audiocommunication Group, Technische Universität Berlin <br>
+ * @brief Main file <br>
+ * <br>
+ * Main file
+ */
+
+#include "granular_synth.h"
---
+++ granular_synth.h
@@ -0,0 +1,13 @@
+/**
+ * @file granular_synth.h
+ * @author Nikita Kretschmar, Adrian Philipp, Micha Strobl, Tim Wennemann <br>
+ * Audiocommunication Group, Technische Universität Berlin <br>
+ * @brief Main file header <br>
+ * <br>
+ * Main file header
+ */
+
+#ifndef granular_synth_h
+#define granular_synth_h
+
+#endif
