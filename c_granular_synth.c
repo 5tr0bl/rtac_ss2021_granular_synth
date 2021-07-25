211916d64f147f62d03709d065d394bc5986b5a1
5tr0bl <m.strobl@campus.tu-berlin.de>
2021-07-25 21:18:47 +0200

    Beginning to separate the main file's code into PureData/C-related files respectively

---
+++ c_granular_synth.c
@@ -0,0 +1,68 @@
+/**
+ * @file c_granular_synth.c
+ * @author your name (you@domain.com)
+ * @brief The C Part of the synthesizer's implementation
+ * @version 0.1
+ * @date 2021-07-25
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "c_granular_synth.h"
+
+void *c_granular_synth_tilde_new(int grain_size_ms)
+{
+    c_granular_synth *x = (c_granular_synth *)malloc(sizeof(c_granular_synth));
+    x->current_grain_index = 0; // den später hochzählen
+
+    // Bitte korrigieren wenn die Umrechnung "ms -> Anzahl Samples" falsch ist!!!
+    x->grain_size_samples = (int)(grain_size_ms / 1000 * SAMPLERATE);
+
+    //Array aus Grains
+    //Länge = filelength / grain_size_samples
+
+    x->windowing_table = (float *) vas_mem_alloc(x->grain_size_samples * sizeof(float));
+
+    //The main inlet is created automatically
+    x->x_out = outlet_new(&x->x_obj, &s_signal);
+
+    return (void *)x;
+}
+
+void c_granular_synth_process(c_granular_synth *x, float *in, float *out, int vector_size)
+{
+    // To-DO
+    // Orientieren an vas_osc_process aus session 5 rtap_osc6~
+}
+
+void c_granular_synth_noteOn(c_granular_synth *x, float frequency, float velocity)
+{
+        //Create Voice, map Midi Key Number to frequency?
+    // Apply Pitch to Signal
+
+    // Use envelope, multiply values between 0-1 with sample volume value -> result = output volume value for voice
+    // if (velocity == 0) -> go into release phase of envelope
+    // -> velocity = 0 means NoteOff-Event
+
+
+}
+
+// Creates a Hanning Window
+// Is hard-coded right now, so it could also be hardcoded when writing samples_tables to Output
+void c_granular_synth_generate_window_function(c_granular_synth *x)
+{
+    int n = 0;
+    while(n < x->grain_size_samples)
+    {
+        //wird SO ins Array geschrieben?..
+        x->windowing_table[n] = 0.54 - 0.46*cosf(2 * M_PI * n / x->grain_size_samples);
+        n++;
+    }
+}
+
+void c_granular_synth_free(c_granular_synth *x)
+{
+    vas_mem_free(x->soundfile_table);
+    free(x);
+}
---
+++ c_granular_synth.h
@@ -0,0 +1,62 @@
+/**
+ * @file c_granular_synth.h
+ * @author Nikita Kretschmar, Adrian Philipp, Micha Strobl, Tim Wennemann <br>
+ * Audiocommunication Group, Technische Universität Berlin <br>
+ * @brief Main file header <br>
+ * <br>
+ * Main file header
+ */
+
+#ifndef c_granular_synth_h
+#define c_granular_synth_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "grain.h"
+#include "envelope.h"
+#include "m_pd.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @struct c_granular_synth
+ * @brief The Purde Data struct of the c_granular_synth~ object. <br>
+ * 
+ */
+
+typedef struct c_granular_synth
+{
+    t_object x_obj;
+    t_sample f;
+    grain *grains;          // Adressen der Grains
+    grain *current_grain;   // which grain are we currently at?
+    t_int current_grain_index;
+    int grain_size_samples;
+    float* soundfile_table;     //Array containing the original soundfile
+    float* windowing_table;  // smoothing window function applied to grain output
+    t_inlet *x_in;          //entweder erzeugte Waveform oder Soundfile
+    t_outlet *x_out;
+} c_granular_synth;
+
+//t_int *c_granular_synth_perform(t_int *w);
+//void c_granular_synth_dsp(c_granular_synth *x, t_signal **sp);
+void c_granular_synth_free(c_granular_synth *x);
+void *c_granular_synth_new(int grain_size_samples);
+//void c_granular_synth_setup(void);
+void c_granular_synth_generate_window_function(c_granular_synth *x);
+
+void c_granular_synth_process(c_granular_synth *x, float *in, float *out, int vector_size);
+void c_granular_synth_noteOn(c_granular_synth *x, float frequency, float velocity);
+
+extern t_float SAMPLERATE;
+
+void grain_generate_window_function(grain *x);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
