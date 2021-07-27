1a720f22b0ca25a834b060e96f3831b8f5a1b246
5tr0bl <m.strobl@campus.tu-berlin.de>
2021-07-27 18:28:22 +0200

    further corrections on erratic method name from previous commit

--- c_granular_synth.c
+++ c_granular_synth.c
@@ -11,7 +11,7 @@
 
 #include "c_granular_synth.h"
 
-c_granular_synth *c_granular_synth_tilde_new(int grain_size_ms)
+c_granular_synth *c_granular_synth_new(int grain_size_ms)
 {
     c_granular_synth *x = (c_granular_synth *)malloc(sizeof(c_granular_synth));
     x->current_grain_index = 0; // den später hochzählen
