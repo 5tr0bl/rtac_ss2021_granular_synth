cc15351129c8838d38c3bb0f4d468060d6273943
AdiBugaddi <adi.philipp@gmail.com>
2021-09-06 18:57:55 +0200

    update window to pull Tims envelope update

--- envelope.c
+++ envelope.c
@@ -59,14 +59,14 @@ float gauss(grain x)
 {
     if (x.grain_size == 0) 
         return 0.0;
-    int fade_size = floor(table_size/10);
-    envelope *window = envelope_new(fade_size, 0, table_size-20, fade_size);
 
-    for(int i =0; i<x->duration; i++)
+    float ;
+
+    for(int i=0; i<x->duration; i++)
     {
-       env_2[i] = np.exp(-((i-grain_size/2)**2/(2*(grain_size*0.2)**2))) * max_ampli
+       env_2[i] = np.exp(-( (i-grain_size/2) **2/ (2*(grain_size*0.2)**2)));
     }
-    
+
     /*
     float p = ((x.current_sample - x.start_sample) - (x.grain_size / 2));
     float c = 6 / x.grain_size;
